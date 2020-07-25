a842a7683836ebe759bca7babb11845053f639d2
ayu-ayushisood <sood.ayushi30@gmail.com>
2020-07-26 01:56:33 +0530

    add placeholder text from constant

--- habit/src/containers/login/index.js
+++ habit/src/containers/login/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View } from 'react-native'
 import CustomTextInput from '../../components/TextInput'
+import * as CONSTANTS from '../../utils/string_constants'
 
 export default function Login(props){
     let [input, setInput] = React.useState('')
     return(
         <View>
             <CustomTextInput 
-                style={{flex: 1}}
-                placeholder = 'Username'
+                placeholder = {CONSTANTS.USERNAME}
                 value = {input}
                 onChangeText = { (value) => setInput(value) }
             />
