ae1257f87620cb63ab61f546f896d486026e2fb7
Karan Asthana <karan@p3infotech.in>
2020-07-26 00:13:58 +0530

    removed repeated import

--- habit/index.js
+++ habit/index.js
@@ -5,6 +5,5 @@
 import {AppRegistry} from 'react-native';
 import AppProvider from './App';
 import {name as appName} from './app.json';
-import 'react-native-gesture-handler';
 
 AppRegistry.registerComponent(appName, () => AppProvider);
