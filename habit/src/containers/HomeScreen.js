343b4497d9da369e224dc17ddbeeca8cf57190c3
Karan Asthana <karan@p3infotech.in>
2020-08-08 20:06:33 +0530

    added very basic bottom tab navigation

--- habit/src/containers/HomeScreen.js
+++ habit/src/containers/HomeScreen.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { View } from 'react-native'
-import TextView from '../components/TextView';
-import { HOME } from '../utils/string_constants';
-import styles from '../styles/common'
+import { Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default function Home(props){
-    let [input, setInput] = React.useState('')
-    return(
-        <View style={styles.page}>
-            <TextView text={HOME} />
-        </View>
-    )
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
+    );
+  }
 }
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+export default createBottomTabNavigator({
+  Home: HomeScreen,
+  Settings: SettingsScreen,
+});
